9dcbaefac059b3bab20887e111835b4e240cb9f7
VictoriqueMoe <loli@victorique.moe>
2024-03-20 22:32:31 +0000

    update for basic bot

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,42 @@
-import { dirname, importx } from "@discordx/importer";
-import { Koa } from "@discordx/koa";
-import type { Interaction, Message } from "discord.js";
-import { IntentsBitField } from "discord.js";
-import { Client } from "discordx";
+import "reflect-metadata";
+import {dirname, importx} from "@discordx/importer";
+import {DIService} from "@discordx/koa";
+import {IntentsBitField} from "discord.js";
+import {Client, tsyringeDependencyRegistryEngine} from "discordx";
 import * as dotenv from "dotenv"
+import {container} from "tsyringe";
+import {moduleRegistrar, registerInstance} from "./src/model/framework/DI/moduleRegistrar.js";
 
-dotenv.config()
+dotenv.config();
 
 const BOT_TOKEN = process.env.BOT_TOKEN
 
-console.log("BOT_TOKEN:", BOT_TOKEN)
-
-export const bot = new Client({
-  // To use only guild command
-  // botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
-
-  // Discord intents
-  intents: [
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.GuildMessageReactions,
-    IntentsBitField.Flags.GuildVoiceStates,
-    IntentsBitField.Flags.MessageContent,
-  ],
-
-  // Debug logs are disabled in silent mode
-  silent: false,
-
-  // Configuration for @SimpleCommand
-  simpleCommand: {
-    prefix: "!",
-  },
-});
-
-bot.once("ready", async () => {
-  // Make sure all guilds are cached
-  // await bot.guilds.fetch();
-
-  // Synchronize applications commands with Discord
-  await bot.initApplicationCommands();
-
-  // To clear all guild commands, uncomment this line,
-  // This is useful when moving from guild commands to global commands
-  // It must only be executed once
-  //
-  //  await bot.clearApplicationCommands(
-  //    ...bot.guilds.cache.map((g) => g.id)
-  //  );
-
-  console.log("Bot started");
-});
-
-bot.on("interactionCreate", (interaction: Interaction) => {
-  bot.executeInteraction(interaction);
-});
-
-bot.on("messageCreate", async (message: Message) => {
-  await bot.executeCommand(message);
-});
-
 async function run() {
-  // The following syntax should be used in the commonjs environment
-  //
-  // await importx(__dirname + "/{events,commands,api}/**/*.{ts,js}");
-
-  // The following syntax should be used in the ECMAScript environment
+  DIService.engine = tsyringeDependencyRegistryEngine.setInjector(container);
   await importx(
-    `${dirname(import.meta.url)}/{events,commands,api}/**/*.{ts,js}`,
+      `${dirname(import.meta.url)}/{events,commands,api}/**/*.{ts,js}`,
   );
-
-  // Let's start the bot
+  moduleRegistrar();
   if (!BOT_TOKEN) {
     throw Error("Could not find BOT_TOKEN in your environment");
   }
-
-  // Log in with your bot token
-  await bot.login(BOT_TOKEN);
-
-  // ************* rest api section: start **********
-
-  // api: prepare server
-  const server = new Koa();
-
-  // api: need to build the api server first
-  await server.build();
-
-  // api: let's start the server now
-  const port = process.env.PORT ?? 3000;
-  server.listen(port, () => {
-    console.log(`discord api server started on ${port}`);
-    console.log(`visit localhost:${port}/guilds`);
+  const client = new Client({
+    intents: [
+      IntentsBitField.Flags.Guilds,
+      IntentsBitField.Flags.GuildMembers,
+      IntentsBitField.Flags.GuildMessages,
+      IntentsBitField.Flags.GuildMessageReactions,
+      IntentsBitField.Flags.GuildVoiceStates,
+      IntentsBitField.Flags.MessageContent,
+    ],
+    silent: false,
+    simpleCommand: {
+      prefix: "!",
+    },
   });
+  registerInstance(client);
 
-  // ************* rest api section: end **********
+  await client.login(BOT_TOKEN);
 }
 
-void run();
+await run();
