75cac66297cd40aee921f81a3d0fdb1004b8e4ec
zmzlois <lois.z@comcord.vision>
2024-03-22 01:20:32 +0000

    moderation

--- packages/bot/src/events/common.ts
+++ packages/bot/src/events/common.ts
@@ -2,9 +2,19 @@ import type { ArgsOf, Client } from "discordx";
 import { Discord, On } from "discordx";
 
 @Discord()
-export class Example {
+export class Moderation {
   @On()
   messageDelete([message]: ArgsOf<"messageDelete">, client: Client): void {
     console.log("Message Deleted", client.user?.username, message.content);
+  }
+
+  @On({ event: "messageCreate" })
+  messageSent([message]: ArgsOf<"messageCreate">, client: Client): void {
+    console.log("Message Sent", client.user?.username, message.content, message.channelId, message.guild?.name)
+
+    if (message.channelId === "1196928792942362665" && message.content.includes("tenor")) {
+      message.delete();
+    }
+
   }
 }
